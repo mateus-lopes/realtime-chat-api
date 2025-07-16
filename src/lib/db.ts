b4764a684cabfee4bdacac83186f9c8c56a84c1e
mateus-lopes <mateusalbano22@gmail.com>
2025-07-16 11:55:42 -0300

    ci: uri hardcode

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async (): Promise<void> => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI!);
+    const conn = await mongoose.connect(
+      "mongodb+srv://mateusalbano20:4y6eOaGKbYfuOC9m@cluster0.1w6ozri.mongodb.net/chat_db?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error:", error);
